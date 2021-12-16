f35a5c8265c8241ffdb4a5aa5c8bd6e3eee51501
seung <38664481+seunggil1@users.noreply.github.com>
2021-12-17 02:33:26 +0900

    add import page

--- category/myApp/urls.py
+++ category/myApp/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('csvImport', views.csvImport, name='csvImport'),
+    path('stu', views.students, name='students'),
+    path('pro', views.professors, name='professors'),
+    path('cou', views.countries, name='countries'),
+    path('cov', views.covid, name='covid')
 ]
--- category/myApp/views.py
+++ category/myApp/views.py
@@ -21,3 +21,78 @@ def index(request):
     except Exception as e:
         print(e)
     return render(request, "myApp/index.html", result)
+
+def csvImportPage(request):
+    return render(request, "myApp/import.html")
+
+def students(request):
+    result = dict()
+    try:
+        with connection.cursor() as cursor:
+            sqlQuery = "Select * from students;"
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+
+            result["type"] = "Student"
+            result["column"] = ('studentID','name','score','country')
+            result["datas"] = row
+
+    except Exception as e:
+        print(e)
+
+    return render(request, "myApp/import.html", result)
+
+def professors(request):
+    result = dict()
+    try:
+        with connection.cursor() as cursor:
+            sqlQuery = "Select * from professors;"
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+
+            result["type"] = "Professor"
+            result["column"] = ('facultyID','name','age','country')
+            result["datas"] = row
+
+    except Exception as e:
+        print(e)
+
+    return render(request, "myApp/import.html", result)
+
+def countries(request):
+    result = dict()
+    try:
+        with connection.cursor() as cursor:
+            sqlQuery = "Select * from countries;"
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+
+            result["type"] = "Country"
+            result["column"] = ('countryName','population','city')
+            result["datas"] = row
+
+    except Exception as e:
+        print(e)
+
+    return render(request, "myApp/import.html", result)
+
+def covid(request):
+    result = dict()
+    try:
+        with connection.cursor() as cursor:
+            sqlQuery = "Select * from covid;"
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+
+            result["type"] = "Covid"
+            result["column"] = ('patientID','city')
+            result["datas"] = row
+
+    except Exception as e:
+        print(e)
+
+    return render(request, "myApp/import.html", result)
+
+def csvImport(request):
+    print(1)
+    return render(request, "myApp/import.html")
