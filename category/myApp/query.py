ff8f3a7e6a24bd3b5249febc006111e0fe5a12cc
seung <38664481+seunggil1@users.noreply.github.com>
2021-12-22 03:33:14 +0900

    encapsulation query function

--- category/myApp/query.py
+++ category/myApp/query.py
@@ -1,43 +1,75 @@
 from django.db import connection
 
+class Query:
+    cursor = connection.cursor()
 
-def studentIndex():
-    try:
-        # cursor = connection.cursor()
-        # 
-        cursor = connection.cursor()
-        sqlQuery = "Select * from students;"
-        cursor.execute(sqlQuery)
-        row = cursor.fetchall()
+    @staticmethod
+    def studentIndex():
+        try:
+            cursor = Query.cursor
+            sqlQuery = "Select * from students;"
+            result = cursor.execute(sqlQuery)
+            row = cursor.fetchall()
 
-        connection.commit()
-        connection.close()
+            connection.commit()
 
-    except Exception as e:
-        return tuple('','','','')
-    return row
+        except Exception as e:
+            return tuple('','','','')
+        return row
 
-def quertType1():
-    sqlQuery = """Select country, avg(score)
+    @staticmethod
+    def quertType1():
+        try:
+            cursor = Query.cursor
+            sqlQuery = """
+                Select country, avg(score)
+                From students
+                group by country;"""
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+            connection.commit()
+
+        except Exception as e:
+            return tuple('','','','')
+        return row
+        
+    @staticmethod
+    def quertType2():
+        # 미완성
+        try:
+            cursor = Query.cursor
+            sqlQuery = """
+                Select country, avg(score)
                 From students
                 group by country"""
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+            connection.commit()
 
-def quertType2():
-    sqlQuery = """Select country, avg(score)
-                From students
-                group by country"""
+        except Exception as e:
+            return tuple('','','','')
+        return row
+        
+    @staticmethod
+    def quertType3():
+        try:
+            cursor = Query.cursor
+            sqlQuery = """
+                Select pro.name, stu.name
+                From (
+                    select name, country, max(score)
+                    from students
+                    group by country
+                ) as stu join
+                (
+                    select name, country, max(age)
+                    from professors
+                    group by country
+                ) as pro on stu.country = pro.country;"""
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+            connection.commit()
 
-def quertType3():
-    sqlQuery = """
-        Select pro.name, stu.name
-        From (
-            select name, country, max(score)
-            from students
-            group by country
-        ) as stu join
-        (
-            select name, country, max(age)
-            from professors
-            group by country
-        ) as pro on stu.country = pro.country
-    """
+        except Exception as e:
+            return tuple('','','','')
+        return row
