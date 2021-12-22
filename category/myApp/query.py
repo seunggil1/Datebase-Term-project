13697204bb413ce18c42a5de3d64b6ba2fb32a49
seung <38664481+seunggil1@users.noreply.github.com>
2021-12-22 15:51:23 +0900

    add query 3,4

--- category/myApp/query.py
+++ category/myApp/query.py
@@ -4,21 +4,7 @@ class Query:
     cursor = connection.cursor()
 
     @staticmethod
-    def studentIndex():
-        try:
-            cursor = Query.cursor
-            sqlQuery = "Select * from students;"
-            result = cursor.execute(sqlQuery)
-            row = cursor.fetchall()
-
-            connection.commit()
-
-        except Exception as e:
-            return tuple('','','','')
-        return row
-
-    @staticmethod
-    def quertType1():
+    def queryType1():
         try:
             cursor = Query.cursor
             sqlQuery = """
@@ -34,14 +20,14 @@ class Query:
         return row
         
     @staticmethod
-    def quertType2():
-        # 미완성
+    def queryType2():
         try:
             cursor = Query.cursor
             sqlQuery = """
-                Select country, avg(score)
-                From students
-                group by country"""
+                Select Countries.city, avg(Students.score)
+                from Students join Countries 
+                on Students.country = Countries.countryName
+                group by city"""
             cursor.execute(sqlQuery)
             row = cursor.fetchall()
             connection.commit()
@@ -51,21 +37,75 @@ class Query:
         return row
         
     @staticmethod
-    def quertType3():
+    def queryType3():
         try:
             cursor = Query.cursor
             sqlQuery = """
-                Select pro.name, stu.name
-                From (
-                    select name, country, max(score)
-                    from students
-                    group by country
-                ) as stu join
-                (
-                    select name, country, max(age)
-                    from professors
-                    group by country
-                ) as pro on stu.country = pro.country;"""
+                Select Professors.name, Students.name
+                From Students, Professors,(
+                        Select stu.country, maxScore, maxAge
+                        From (
+                            select country, max(score) as maxScore
+                            from students
+                            group by country
+                        ) as stu join
+                        (
+                            select country, max(age) as maxAge
+                            from professors
+                            group by country
+                        ) as pro on stu.country = pro.country
+                    ) as maxInfo
+                Where Students.country = Professors.country
+                    and Students.country = maxInfo.country
+                    and Students.score = maxInfo.maxScore
+                    and Professors.age = maxInfo.maxAge;"""
+            cursor.execute(sqlQuery)
+            row = cursor.fetchall()
+            connection.commit()
+
+        except Exception as e:
+            return tuple('','','','')
+        return row
+
+
+    @staticmethod
+    def queryType4():
+        try:
+            cursor = Query.cursor
+            sqlQuery = """
+                Select Pro.name, Stu.name
+                FROM (
+                        Select facultyID, name, age, ProfessorsWithCity.city
+                        From (Select *
+                                From Professors join Countries
+                                on Professors.country = Countries.countryName
+                            ) as ProfessorsWithCity,
+                            (
+                                Select max(age) as maxAge, city
+                                From Professors join Countries
+                                on Professors.country = Countries.countryName
+                                group by city
+                            ) as cityMaxAge
+                        Where ProfessorsWithCity.age = cityMaxAge.maxAge
+                            and ProfessorsWithCity.city = cityMaxAge.city
+                    ) as Pro 
+                    join
+                    (
+                        Select studentID, name, score, studentsWithCity.city
+                        From (Select *
+                                From Students join Countries
+                                on Students.country = Countries.countryName
+                            ) as studentsWithCity,
+                            (
+                                Select max(score) as maxScore, city
+                                From Students join Countries
+                                on Students.country = Countries.countryName
+                                group by city
+                            ) as cityMaxScore
+                        Where studentsWithCity.score = cityMaxScore.maxScore
+                            and studentsWithCity.city = cityMaxScore.city
+                    ) as Stu
+                    on Pro.city = Stu.city;"""
             cursor.execute(sqlQuery)
             row = cursor.fetchall()
             connection.commit()
